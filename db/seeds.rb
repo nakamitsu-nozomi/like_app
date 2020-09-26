68c13ea8b7583d66fb38bb125e6470b7bdde0a7c
nozomi <kkk@docomo.ne.jp>
2020-09-26 16:28:49 +0900

    投稿とユーザーの紐付け完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+## ログイン時に使用する「メールアドレス」と「パスワード」を定数化して上に配置
+EMAIL = "test@example.com"
+PASSWORD ="password"
+
+
+#データを全削除
+User.destroy_all
+
+# 以下，開発用の初期データ
+user1 = User.create!(email: "satou@example.com", password: "password")
+user2 = User.create!(email: "suzuki@example.com", password: "password")
+user3 = User.create!(email: "tanaka@example.com", password: "password")
+
+
+# user2.posts.create!(content: "おはよう")
+# user2.posts.create!(content: "こんにちは")
+# user3.posts.create!(content: "こんばんは")
+
+#問題２
+user2.posts.create!(content: "沖縄そば")
+user1.posts.create!(content: "海ぶどう")
+user3.posts.create!(content: "ゴーヤチャンプル")
+user3.posts.create!(content: "サータアンダギー")
+user1.posts.create!(content: "タコライス")
+
+# ログイン時に使用するアカウント（変数への代入は不要）
+User.create!(email: EMAIL,password: PASSWORD)
+puts "初期データの投入に成功しました"
