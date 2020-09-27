3ed666f4f84964c0fab244d4fa64859e78088d70
nozomi <kkk@docomo.ne.jp>
2020-09-27 10:39:39 +0900

    20200926130953間違えてrails db:migrate

--- db/migrate/20200926130953_add_likes_count_to_posts.rb
+++ db/migrate/20200926130953_add_likes_count_to_posts.rb
@@ -1,11 +1,7 @@
 class AddLikesCountToPosts < ActiveRecord::Migration[6.0]
-  def up
-    add_column :posts, :likes_count, :integer
-    Post.find_each{|post| Post.reset_counters(post.id, :likes_count)}
-  end
-  
-  def down
-    remove_column :posts, :likes_count, :integer
-  end
+  # def change
+  #   # ***** デフォルト値を設定 *****
+  #   add_column :posts, :likes_count, :integer, default: 0
+  # end
   
 end
